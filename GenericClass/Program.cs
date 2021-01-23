3b7ed5199bbc6e12944c4e7cc7610d23f911bae0
Ferdi Kaya <ferdi_kaya55@hotmail.com>
2021-01-23 19:52:41 +0300

    C# beginner examples added.

--- GenericClass/Program.cs
+++ GenericClass/Program.cs
@@ -8,13 +8,13 @@ namespace Methods
         static void Main(string[] args)
         {
             MyList<int,string> myList = new MyList<int, string>();
-            myList.Add(34, "Istanbul");
+            myList.Add(34, "İstanbul");
             myList.Add(06, "Ankara");
             myList.Add(54, "Sakarya");
             myList.Add(55, "Samsun");
             myList.Add(35, "Izmir");
 
-            Console.WriteLine("Il Sayısı: " + myList.Count);
+            Console.WriteLine("Il Sayısı: " myList.Count);
             
 
         }
